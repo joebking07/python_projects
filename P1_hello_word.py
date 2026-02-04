7871d6abca0a9d576e9fd30b903fda7200deb0d8
joebking07 <agbessijoe2006@gmail.com>
2026-02-04 20:24:02 +0000

    first commiit

---
+++ P1_hello_word.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+def greet(nom):
+    date = datetime.now().date()
+    heure = datetime.now().hour
+    if 5 < heure <12:
+        t_greet = 'good morning'
+    if 12 < heure < 18:
+        t_greet = 'good evening'
+    if 18 < heure < 23:
+        t_greet = 'good nigth'
+    display_user(t_greet , nom , date)
+
+def display_user(t_greet , nom , date):
+    print("===== HELLO WORD =====")
+    print(f"nous somme le {date},")
+    print(f"{t_greet} {nom}. Welcome in my first project")
+
+def get_name():
+    tentative = 0
+    while True:
+        try:
+            nom = input("entrez votre nom : ").strip()
+            if nom =="":
+                raise ValueError
+            if nom :
+                greet(nom)
+                break
+            else :
+                tentative += 1
+                print("erreur")
+                if tentative == 5:
+                    print("vous avez atteint la tentative maximale")
+                    break
+        except ValueError:
+            print("ERROR: vous avez fait une erreur!!")
+
+if __name__ == "__main__":
+    get_name()
+
+
+    
+    
+        
+            
+                
+
+        
+        
+    
+
