13cc52df0ac08c9557b83780b265bf02faef43cb
joebking <agbessijoe2006@gmail.com>
2026-02-05 15:03:15 +0000

    second programe of all project

--- P2_calculator.py
+++ P2_calculator.py
@@ -3,7 +3,7 @@ def advanced_calculator():
     historique = []
     while True:
         calc = 'bienvenue dans votre calculatrice avancée'
-        print(f"{calc:=^50}")  
+        print(f"{calc:=^51}")  
 
         print("1: Addition (+)")
         print("2: Soustraction (-)")
@@ -13,33 +13,37 @@ def advanced_calculator():
         print("6: Modulo (%)")
         print("7: Afficher l'historique")
         print("8: Quitter")
+
         try:
-            choix = int(input("entrez votre choix :"))
-            if choix == "+" or choix == "1" : choix == 1
-            elif choix == "-" or choix =="2" : choix == 2
-            elif choix == "*" or choix == "3" : choix == 3
-            elif choix == "/" or choix == "4" : choix == 4 
-            elif choix == "^" or choix == "**" or  choix == "5" : choix == 5
-            elif choix == "%" or choix =="6" : choix == 6
-            elif choix == "7" : choix == 7
-            elif choix == "8" : choix == 8
+            choix = input("entrez votre choix :")
+            if choix == "+" or choix == "1" : choix = 1
+            elif choix == "-" or choix =="2" : choix = 2
+            elif choix == "*" or choix == "3" : choix = 3
+            elif choix == "/" or choix == "4" : choix = 4 
+            elif choix == "^" or choix == "**" or  choix == "5" : choix = 5
+            elif choix == "%" or choix =="6" : choix = 6
+            elif choix == "7" : choix = 7
+            elif choix == "8" : choix = 8
+            else: raise ValueError
             choix = int(choix)
         except ValueError:
             print(" Erreur : veullier réesayer")
+
         if choix == 8:
             try:
-                quitter = input("voulez vous quitter? (oui/non) :")
+                quitter = input("voulez vous quitter? \u2192 (oui/non) :")
                 if quitter == "oui":
+                    quit = " MERCI ET A BIENTOT "
+                    print(F"{quit:_^50}")
                     break
                 else:
                     continue
             except ValueError:
-                quit = " AUREVOIR ET A BIENTOT "
-                print(F"{quit:_^50}")
+                print("Entrée  soit oui ou non")
 
         if choix == 7:
             if not historique:
-                print("vous n'avez aucun choix dans l'historique")
+                print("vous n'avez aucun calcul dans l'historique")
             else:
                 print("l'historique des cinq derniers calculs")
                 for calcs in historique:
@@ -47,10 +51,11 @@ def advanced_calculator():
             continue
 
         try:
-            nbr1 = float(input("entrez le premier nombre: "))
-            nbr2 = float(input("entrez le deuxième nombre: "))
+            nbr1 = float(input("entrez le premier nombre: ").strip())
+            nbr2 = float(input("entrez le deuxième nombre: ").strip())
         except ValueError:
             print("Erreur : entrée de nombre invalide.")
+            continue
         if choix == 1:
                 result = nbr1 + nbr2
                 clcf = "addition"
@@ -68,16 +73,16 @@ def advanced_calculator():
                 print("Erreur: aucun nombre n'est divisible par zero")
                 raise ZeroDivisionError
             else:
-                    result = nbr1 / nbr2
-                    result = round(result,2)
-                    clcf = "division"
-                    sn_calc = "/"
-        elif choix == 5:
-                nbr2 = int(nbr2)
-                result = nbr1 ** nbr2
+                result = nbr1 / nbr2
                 result = round(result,2)
-                clcf = "puissance"
-                sn_calc ="^" 
+                clcf = "division"
+                sn_calc = "/"
+        elif choix == 5:
+            nbr2 = int(nbr2)
+            result = pow(nbr1,nbr2)
+            result = round(result,2)
+            clcf = "puissance"
+            sn_calc ="^" 
         elif choix == 6:
             if nbr2 == 0: 
                 print("Erreur: aucun nombre n'est divisible par zero")
@@ -87,23 +92,23 @@ def advanced_calculator():
                 result = round(result,2)
                 clcf = "modulo"
                 sn_calc = "%"
-        print(f"votre calcul : {clcf} a pour résultat:{result} ")
-                
+        print(f"votre calcul : {clcf} a pour résultat:{result:.2f} ")
+                    
         historique.append(f"{nbr1} {sn_calc} { nbr2} = {result:.2f}")
         if len(historique) > 5:
             historique.pop(0)
-                
- 
+                    
+    
 if __name__== "__main__":
     advanced_calculator()   
-                
-        
-                      
                     
+            
+                        
+                        
+                            
+
                         
 
-                    
 
 
-
-        
+            
